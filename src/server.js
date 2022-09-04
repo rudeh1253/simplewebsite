7e871b4bc056b22151272c7285d54e8585d89156
PGD <rudeh1253@gmail.com>
2022-09-04 17:34:13 +0900

    Update views related to list and detail of an element in list.

--- src/server.js
+++ src/server.js
@@ -14,6 +14,8 @@ app.get('/', (req, resp) => {
     resp.render('index.ejs');
 });
 
+app.use('/public', express.static('public'));
+
 app.use('/', require('./write'));
 app.use('/', require('./list'));
 app.use('/', require('./account'));
